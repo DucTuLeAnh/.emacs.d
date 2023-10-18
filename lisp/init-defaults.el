06bf46dc5f4179846d15d150b8a4ca93cf74f07c
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2023-10-18 21:59:22 +0200

    feat: add dracula theme

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -8,4 +8,15 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
+(unless (package-installed-p 'dracula-theme)
+  (package-install 'dracula-theme))
+
+(load-theme 'dracula t)
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+
 (provide 'init-defaults)

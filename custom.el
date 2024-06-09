73734034ce3cdcc8e58d668c2ba1a1bd1d49def6
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2024-06-09 17:29:46 +0200

    feat(lsp): add dap mode for debugging capabilities

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/git-repo/org-todos/all-todos.org"))
  '(package-selected-packages
-   '(slime yaml-mode treemacs web-mode which-key lsp-mode magit counsel projectile undo-tree evil)))
+   '(slime yaml-mode treemacs web-mode which-key lsp-mode magit counsel projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -5,4 +5,28 @@
 ;;(setq xref-auto-jump-to-first-xref t)
 (require 'lsp-mode)
 
+
+(require 'dap-mode)
+
+(dap-mode 1)
+;; The modes below are optional
+(dap-ui-mode 1)
+;; enables mouse hover support
+(dap-tooltip-mode 1)
+;; use tooltips for mouse hover
+;; if it is not enabled `dap-mode' will use the minibuffer.
+(tooltip-mode 1)
+;; displays floating panel with debug buttons
+;; requies emacs 26+
+(dap-ui-controls-mode 1)
+
+(require 'dap-node)
+
+(dap-node-setup)
+
+(require 'dap-chrome)
+(dap-chrome-setup)
+
+
+
 (provide 'init-lsp)

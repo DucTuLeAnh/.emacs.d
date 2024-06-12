594d4c715d747fe310bf5bc59ca9f09477f99aad
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2024-06-12 16:55:29 +0200

    feat(counsel): make ag search unrestricted

--- lisp/init-counsel.el
+++ lisp/init-counsel.el
@@ -6,7 +6,9 @@
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 
+(setq counsel-ag-base-command "ag -u --nocolor --nogroup %s")
 ;; setup counsel key bindings
 (global-set-key (kbd "C-c s") 'counsel-ag)
+
 
 (provide 'init-counsel)
